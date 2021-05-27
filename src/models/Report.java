92af39d15edcfb55c63bb0c444d6cb8ba042d616
asami.moriyasu <a.moriyasu01@gmail.com>
2021-05-27 20:59:02 +0900

    Add Report index

--- src/models/Report.java
+++ src/models/Report.java
@@ -23,10 +23,9 @@ import javax.persistence.Table;
     ),
     @NamedQuery(
         name = "getReportsCount",
-        query = "SELECT r FROM Report AS r ORDER BY r.id DESC"
+        query = "SELECT COUNT(r) FROM Report AS r"
     ),
 })
-
 @Entity
 public class Report {
     @Id
@@ -41,18 +40,18 @@ public class Report {
     @Column(name = "report_date", nullable = false)
     private Date report_date;
 
-    @Column(name = "title", length = 225, nullable = false)
+    @Column(name = "title", length = 255, nullable = false)
     private String title;
 
     @Lob
     @Column(name = "content", nullable = false)
     private String content;
 
-    @Column(name = "create_at", nullable = false)
-    private Timestamp create_at;
+    @Column(name = "created_at", nullable = false)
+    private Timestamp created_at;
 
-    @Column(name = "update_at", nullable = false)
-    private Timestamp update_at;
+    @Column(name = "updated_at", nullable = false)
+    private Timestamp updated_at;
 
     public Integer getId() {
         return id;
@@ -94,21 +93,19 @@ public class Report {
         this.content = content;
     }
 
-    public Timestamp getCreate_at() {
-        return create_at;
+    public Timestamp getCreated_at() {
+        return created_at;
     }
 
-    public void setCreate_at(Timestamp create_at) {
-        this.create_at = create_at;
+    public void setCreated_at(Timestamp created_at) {
+        this.created_at = created_at;
     }
 
-    public Timestamp getUpdate_at() {
-        return update_at;
+    public Timestamp getUpdated_at() {
+        return updated_at;
     }
 
-    public void setUpdate_at(Timestamp update_at) {
-        this.update_at = update_at;
+    public void setUpdated_at(Timestamp updated_at) {
+        this.updated_at = updated_at;
     }
-
-
 }
